8dc4c5ce176a72c22c6e25347c7e612e1ad2d9e0
gtorok <hunhm1@gmail.com>
2015-09-12 09:48:26 -0700

    awesome intro graphic

--- src/menu.js
+++ src/menu.js
@@ -6,24 +6,29 @@ var MenuState = function(game) {
 };
 
 MenuState.prototype.preload = function() {
+    this.game.load.image('ermin', 'data/ermin.png');
     this.game.load.bitmapFont('ermin', 'data/ermin/font.png', 'data/ermin/font.fnt');
     this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.game.scale.pageAlignVertically = true;
 };
 
 MenuState.prototype.create = function() {
-    this.title_text = this.game.add.bitmapText(this.game.width/2, 100, 'ermin', "Ermin's Quest", 48);
+    this.title_text = this.game.add.bitmapText(this.game.width/2, 60, 'ermin', "Ermin's Quest", 48);
     this.title_text.tint = 0xff8800;
     this.title_text.anchor.x = 0.5;
     this.title_text.anchor.y = 0.5;
-    this.title_text2 = this.game.add.bitmapText(this.game.width/2, 160, 'ermin', "Heads on Stick, Inc (c) 2015", 16);
+    this.title_text2 = this.game.add.bitmapText(this.game.width/2, 16, 'ermin', "Heads on Stick, Inc (c) 2015", 16);
     this.title_text2.tint = 0x888888;
     this.title_text2.anchor.x = 0.5;
     this.title_text2.anchor.y = 0.5;
-    this.start_text = this.game.add.bitmapText(this.game.width/2, this.game.height - 100, 'ermin', "Press space or click to start", 16);
+    this.start_text = this.game.add.bitmapText(this.game.width/2, this.game.height - 70, 'ermin', "Press space or click to start", 16);
     this.start_text.tint = 0x0088ff;
     this.start_text.anchor.x = 0.5;
     this.start_text.anchor.y = 0.5;
+
+    this.ermin = this.game.add.sprite(this.game.width/2, this.game.height/2, "ermin")
+    this.ermin.anchor.x = 0.5;
+    this.ermin.anchor.y = 0.5;
 };
 
 MenuState.prototype.update = function() {
